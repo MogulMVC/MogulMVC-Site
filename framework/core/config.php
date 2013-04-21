a4a16ed27c8a3faeb7c641a5a690e2fbbb6dff5c
Alan James <alanjames1987@gmail.com>
2013-04-21 13:12:15 -0400

    Commit

--- framework/core/config.php
+++ framework/core/config.php
@@ -11,7 +11,7 @@ if (!defined('SERVER_ROOT')) {
 	exit ;
 }
 
-define('FRAMEWORK_VERSION', '130418');
+define('FRAMEWORK_VERSION', '130421');
 
 define('FRAMEWORK_IMG', 'img');
 define('FRAMEWORK_LIB_CSS', 'lib/css');
