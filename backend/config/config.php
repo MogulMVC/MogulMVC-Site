e6d5eddd2fdd20f361b88d597ad09f20f168400c
Alan James <alanjames1987@gmail.com>
2013-05-15 00:28:12 -0400

    Commit

--- backend/config/config.php
+++ backend/config/config.php
@@ -11,7 +11,7 @@ define('APPLICATION_COMPANY', 'Mogul MVC');
 define('APPLICATION_NAME', 'Mogul MVC');
 define('APPLICATION_DESCRIPTION', 'Mogul MVC is an MVC framework and UI library written in PHP, jQuery, and Sass.');
 define('APPLICATION_PASSWORD', 'mogulpass321456');
-define('APPLICATION_VERSION', '130513');
+define('APPLICATION_VERSION', '130515');
 
 // Environment
 // 'development' 'production'
