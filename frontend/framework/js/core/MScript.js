14a03b6e2c5fa2ba5236dd4f10c06157b2117357
Alan James <alanjames1987@gmail.com>
2013-05-18 22:12:46 -0400

    Commit

--- frontend/framework/js/core/MScript.js
+++ frontend/framework/js/core/MScript.js
@@ -1765,25 +1765,6 @@ var MToolBar = (function() {
 })();
 (function() {
 
-	var MColor = {
-		fadeHex : function(hex, hex2, ratio) {
-			var r = hex >> 16;
-			var g = hex >> 8 & 0xFF;
-			var b = hex & 0xFF;
-
-			r += ((hex2 >> 16) - r) * ratio;
-			g += ((hex2 >> 8 & 0xFF) - g) * ratio;
-			b += ((hex2 & 0xFF) - b) * ratio;
-
-			return (r << 16 | g << 8 | b);
-		}
-	};
-
-	window.MColor = MColor;
-
-})();
-(function() {
-
 	var MCookie = {
 		set : function(name, value, expires, path, domain, secure) {
 			var today = new Date();
