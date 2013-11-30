002f428e357fcef7c46cef6027206589dfafaf8a
Alan James <ajames@Landmine.(none)>
2013-11-30 07:19:07 -0500

    Transfering to GitHub.

--- backend/config/config.php
+++ backend/config/config.php
@@ -27,7 +27,7 @@ define('APPLICATION_ERROR_404_CONTROLLER', 'Error_404');
 
 // Templates
 //File locations start within the application view directory
-define('APPLICATION_HEAD', 'template/head');
-define('APPLICATION_HEADER', 'template/header');
-define('APPLICATION_FOOTER', 'template/footer');
-define('APPLICATION_FOOT', 'template/foot');
+define('APPLICATION_HEAD', '_template/head');
+define('APPLICATION_HEADER', '_template/header');
+define('APPLICATION_FOOTER', '_template/footer');
+define('APPLICATION_FOOT', '_template/foot');
