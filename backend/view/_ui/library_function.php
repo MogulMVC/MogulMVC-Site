6265fc838a7f7fb933381691a02baa65979a7cee
Alan James <alanjames1987@gmail.com>
2013-12-26 12:50:16 -0500

    Commit

--- backend/view/_ui/library_function.php
+++ backend/view/_ui/library_function.php
@@ -1,8 +1,3 @@
-<?php if(isset($added)):?>
-	
-<span class="right">Added in Version <span class="MTextBold library-version-<?php echo $added; ?>"><?php echo $added;?></span></span>
-<?php endif; ?>
-
 <h3><?php echo $title; ?></h3>
 
 <hr />
