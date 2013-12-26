2ec5cd3965f8eb9ba7106b8bbf1a41fabf387063
Alan James <alanjames1987@gmail.com>
2013-12-26 10:09:51 -0500

    Updated some documentation and added download tar.gz files.

--- backend/view/_template/header.php
+++ backend/view/_template/header.php
@@ -57,6 +57,6 @@
 		
 		<span class="right" style="margin-right: 140px;">Current Version : <span class="MTextBold">1.0.0</span></span>
 		
-		<a href="https://github.com/MogulMVC"><img style="position: fixed; top: 0; right: 0; border: 0; z-index:10000" src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" alt="Fork me on GitHub"></a>
+		<a href="https://github.com/MogulMVC"><img style="position: fixed; top: 0; right: 0; border: 0; z-index:10000" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
 
 	</nav><!--MHeader-->
