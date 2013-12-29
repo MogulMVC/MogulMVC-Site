c9a0ace83571a288b5dc385a800ff69953f10cc1
Alan James <alanjames1987@gmail.com>
2013-12-28 21:33:06 -0500

    Commit

--- backend/view/_template/header.php
+++ backend/view/_template/header.php
@@ -23,7 +23,7 @@
 		<li>
 			<span>PHP</span>
 			<ul>
-				<li><a href="/misc/php/MogulPHP-1.0.0.tar.gz">Download MogulPHP</a></li>
+				<li><a href="/misc/php/MogulPHP-1.0.0.tar.gz">Download MogulPHP v1.0.1</a></li>
 				<li><a href="/php/configuration">Configuration</a></li>
 				<li><a href="/php/models">Models</a></li>
 				<li><a href="/php/views">Views</a></li>
@@ -36,7 +36,7 @@
 		<li>
 			<span>JavaScript</span>
 			<ul>
-				<li><a href="/misc/js/MogulJS-1.0.0.tar.gz">Download MogulJS</a></li>
+				<li><a href="/misc/js/MogulJS-1.0.5.tar.gz">Download MogulJS v1.0.5</a></li>
 				<li><a href="/js/managers">Managers</a></li>
 				<li><a href="/js/ui_elements">UI Elements</a></li>
 				<li><a href="/js/libraries">Utility Libraries</a></li>
@@ -47,7 +47,7 @@
 		<li>
 			<span>CSS</span>
 			<ul>
-				<li><a href="/misc/css/MogulCSS-1.0.0.tar.gz">Download MogulCSS</a></li>
+				<li><a href="/misc/css/MogulCSS-1.0.0.tar.gz">Download MogulCSS v1.0.0</a></li>
 				<li><a href="/css/typography">Typography</a></li>
 				<li><a href="/css/icons">Icons</a></li>
 				<li><a href="/css/ui_elements">UI Elements</a></li>
