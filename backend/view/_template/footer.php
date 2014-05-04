c8ad65a8ebaab3be54ec0b590dbea0c3a8ada78d
Alan James <alanjames1987@gmail.com>
2014-05-04 18:00:38 -0400

    Adds author information in the footer.

--- backend/view/_template/footer.php
+++ backend/view/_template/footer.php
@@ -3,5 +3,6 @@
 </div><!--MMainWindow-->
 
 <footer id="MFooter">
+	<span class="left">By <a href="http://alanjames1987.com">Alan James</a></span>
 	<span>&copy; 2012 - <?php echo date('Y') . ' ' . APPLICATION_COMPANY; ?></span>
 </footer>
